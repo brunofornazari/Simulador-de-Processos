35c34a3b50428ab84e8323f74b5b25fc547c1715
boiling-peak-2036 <bruno_fornazari@hotmail.com>
2015-04-25 18:28:07 -0300

    Inicio Main 2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import workStation.*;
 import javax.swing.JOptionPane;
 public class Main {
+    static ArrayList<Processo> fila = new ArrayList<Processo>();
+    //ArrayList<int> gant = new ArrayList<int>();
     public static void main(String[] args) {
-        ArrayList<Processo> fila = new ArrayList<>();
-        ArrayList<int> gant = new ArrayList<>();
+        
+    }
+
+    public static void novoProcesso(Processo p){
+        fila.add(p);
     }
 }
